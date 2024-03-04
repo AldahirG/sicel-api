7708443254a79eaa2b9f707b74b10eec83acb57a
jon013002 <jonmtz3002@gmail.com>
2024-03-04 12:52:16 -0600

    Add importation of routes on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 
+// Importar rutas
 import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/user.routes.js';
 import promoterRoutes from './routes/promoter.routes.js';
+import roleRoutes from './routes/role.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 const app = express();
 
@@ -20,9 +22,11 @@ app.options("*", cors());
 
 app.use(express.json());
 
+// Mandar a traer las rutas
 app.use('/api', authRoutes);
-app.use('/api', userRoutes);
 app.use('/api', promoterRoutes);
+app.use('/api', roleRoutes);
+app.use('/api', userRoutes);
 
 app.listen(3000);
 console.log("Servidor corriendo en puerto: ", 3000);
