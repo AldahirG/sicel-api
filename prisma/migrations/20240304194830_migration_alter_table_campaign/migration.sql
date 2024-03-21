cd914eaef6f00387499c9e5d924dcfe7ff102cf4
jon013002 <jonmtz3002@gmail.com>
2024-03-21 15:16:03 -0600

    Reset migrations

--- prisma/migrations/20240304194830_migration_alter_table_campaign/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Campaign_name_key` ON `campaign`;
-
--- AlterTable
-ALTER TABLE `campaign` MODIFY `status` INTEGER NOT NULL DEFAULT 1;
--- prisma/migrations/20240304213045_migration_table_grade/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Grade` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(20) NOT NULL,
-
-    UNIQUE INDEX `Grade_name_key`(`name`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
