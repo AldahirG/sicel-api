22b17eeb779c85c4c57d85947322137e31be7b52
jon13002 <jonmtz3002@gmail.com>
2024-04-30 10:18:41 -0600

    Add env variable to cors

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: [process.env.ALLOWED_ORIGIN],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
