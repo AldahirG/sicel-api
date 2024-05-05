8e3e68f92097c971578375809a93c7523ecc7f9b
Z0KRI <david1166@live.com.mx>
2024-05-05 15:38:49 -0600

    ⚡️ Feat: Implementacion de HandlerError

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { APP_FILTER } from '@nestjs/core';
+import { PrismaClientErrorFilter } from './common/exceptions/prisma-exception.filter';
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: PrismaClientErrorFilter,
+    },
+  ],
 })
 export class AppModule { }
