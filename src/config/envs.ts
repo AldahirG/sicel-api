d4ffbc221f6b832c01c7ab70608ebf5a8f220198
Z0KRI <david1166@live.com.mx>
2024-05-04 23:47:12 -0600

    🔧 Feat: Configuración de env

---
+++ src/config/envs.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvsVars {
+  JWT_SECRET: string;
+  DATABASE_URL: string;
+}
+
+const envSchema = joi
+  .object({
+    DATABASE_URL: joi.string().required(),
+    JWT_SECRET: joi.string().required()
+  })
+  .unknown();
+
+const { error, value } = envSchema.validate({
+  ...process.env
+});
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const env: EnvsVars = value;
+
+export const envs = {
+  jwtSecret: env.JWT_SECRET,
+};
