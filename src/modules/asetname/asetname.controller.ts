859d0b1518d239075255d4ac24f32b01004175f4
Z0KRI <david1166@live.com.mx>
2024-05-05 19:21:51 -0600

    🔒️ Feat: Protegiendo rutas

--- src/modules/asetname/asetname.controller.ts
+++ src/modules/asetname/asetname.controller.ts
@@ -1,28 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param,  Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Query, UseGuards } from '@nestjs/common';
 import { AsetnameService } from './asetname.service';
 import { CreateAsetnameDto } from './dto/create-asetname.dto';
 import { UpdateAsetnameDto } from './dto/update-asetname.dto';
 import { PaginationFilterDto } from 'src/common/dto/pagination-filter.dto';
+import { JwtAuthGuard } from 'src/common/guards/auth.guard';
 
 @Controller('aset-name')
 export class AsetnameController {
   constructor(private readonly asetnameService: AsetnameService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createAsetnameDto: CreateAsetnameDto) {
     return this.asetnameService.create(createAsetnameDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(@Query() params: PaginationFilterDto) {
     return this.asetnameService.findAll(params);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.asetnameService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAsetnameDto: UpdateAsetnameDto) {
     return this.asetnameService.update(+id, updateAsetnameDto);
