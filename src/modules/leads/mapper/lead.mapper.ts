46855d721e48aa1fd93d56dde552a6b755a6efc6
Z0KRI <david1166@live.com.mx>
2024-07-03 00:15:39 -0600

    ⚡️ Feat: Agregando interfaz de LeadMapper

--- src/modules/leads/mapper/lead.mapper.ts
+++ src/modules/leads/mapper/lead.mapper.ts
@@ -1,5 +1,7 @@
+import { LeadMapper } from "../interfaces/lead-mapper.interface"
+
 export class LeadResource {
-	static map(lead: any) {
+	static map(lead: any): LeadMapper {
 		return {
 			id: lead.id,
 			grade: lead.grade,
@@ -49,56 +51,8 @@ export class LeadResource {
 		}
 	}
 
-	static collection(leads: any[]) {
-		const data = leads.map((lead) => ({
-			id: lead.id,
-			grade: lead.grade,
-			createAt: lead.createAt,
-			updateAt: lead.updateAt,
-			dateContact: lead.dateContact,
-			scholarship: lead.scholarship,
-			semester: lead.semester,
-			cycle: {
-				id: lead.Cycle?.id,
-				name: lead.Cycle?.name,
-				cycle: lead.Cycle?.cycle,
-			},
-			reference: {
-				type: lead.reference?.type,
-				name: lead.reference?.name,
-				dataSource: lead.reference?.dataSource,
-			},
-			information: {
-				name: lead.information?.name,
-				genre: lead.information?.genre,
-				careerInterest: lead.information?.careerInterest,
-				formerSchool: lead.information?.formerSchool,
-				typeSchool: lead.information?.typeSchool,
-				enrollmentStatus: lead.information?.enrollmentStatus,
-				followUp: lead.information?.followUp,
-			},
-			campaign: {
-				id: lead.campaign?.id,
-				name: lead.campaign?.name,
-			},
-			asetName: {
-				id: lead.asetName?.id,
-				name: lead.asetName?.name,
-			},
-			phones: lead.phones?.map((i) => i.telephone),
-			emails: lead.emails?.map((i) => i.email),
-			address: {
-				city: lead.city?.name,
-				state: lead.city?.state.name,
-				country: lead.city?.state.country.name,
-			},
-			promoter: {
-				id: lead.user?.id,
-				name: lead.user?.name,
-				paternalSurname: lead.user?.paternalSurname,
-				maternalSurname: lead.user?.maternalSurname,
-			},
-		}))
+	static collection(leads: any[]): LeadMapper[] {
+		const data = leads.map((lead) => this.map(lead))
 		return data
 	}
 }
